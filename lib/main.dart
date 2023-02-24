5e3e77c0e327d912ecd394f6d2d657d996a19bba
Hieu <tuconghieu1998@gmail.com>
2023-02-24 15:01:15 +0700

    add local storage, check show intro screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/core/constants/color_constants.dart';
+import 'package:flutter_app/core/helpers/local_storage_helper.dart';
 import 'package:flutter_app/representation/screens/splash_screen.dart';
 import 'package:flutter_app/routes.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter();
+  await LocalStorageHelper.initLocalStorageHelper();
   runApp(const MyApp());
 }
 
--- lib/representation/screens/splash_screen.dart
+++ lib/representation/screens/splash_screen.dart
@@ -3,7 +3,9 @@ import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_app/core/helpers/asset_helper.dart';
 import 'package:flutter_app/core/helpers/image_helper.dart';
+import 'package:flutter_app/core/helpers/local_storage_helper.dart';
 import 'package:flutter_app/representation/screens/intro_screen.dart';
+import 'package:flutter_app/representation/screens/main_screen.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -24,9 +26,16 @@ class _SplashScreenState extends State<SplashScreen> {
   }
 
   void redirectIntroScreen () async {
+    final ignoreIntroScreen = LocalStorageHelper.getValue("ingoreIntroScreen") as bool ?;
     await Future.delayed(const Duration(seconds: 2));
     // ignore: use_build_context_synchronously
-    Navigator.of(context).pushNamed(IntroScreen.routeName);
+    if(ignoreIntroScreen != null && ignoreIntroScreen) {
+      Navigator.of(context).pushNamed(MainScreen.routeName);
+    }
+    else {
+      LocalStorageHelper.setValue("ingoreIntroScreen", true);
+      Navigator.of(context).pushNamed(IntroScreen.routeName);
+    }
   }
 
   @override
